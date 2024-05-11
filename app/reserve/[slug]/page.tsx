4778743d18c984dd507c19739a653bce6b24ec79
Segcal <seguncaleb3@gmail.com>
2024-05-11 18:35:02 +0100

    new

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,9 +1,12 @@
+
 import Header from "./components/Header";
 import FormControl from "./components/FormControl";
+import NavBar from "../../components/NavBar";
 
 const Reserve = () => {
   return (
     <div className="border-t h-screen">
+      <NavBar />
       <div className="py-9 w-3/5 m-auto">
         <Header />
         <FormControl />
